7880d24f25df2e93d25a29a5754ce0d8b97880f4
Edon Sekiraqa <edon@frakton.com>
2020-09-23 11:27:28 +0200

    Change author

--- teb-kosovo-woocommerce-plugin.php
+++ teb-kosovo-woocommerce-plugin.php
@@ -4,7 +4,7 @@ Plugin Name:       Teb Kosovo WooCommerce Payment Gateway
 Plugin URI:        #
 Description:       A plugin that helps the customer to setup their Teb Payment Provider in their WooCommerce Shop. This plugin is especially for Kosovo and might not work on all countries.
 Version:           1.0.0
-Author:            Edon Sekiraqa
+Author:            FRAKTON L.L.C
 */;
 if (!defined('ABSPATH')){
     exit;
